60c8bf2f77ca1145bd72393ba750a77042133483
mgnush <mhjorthen@gmail.com>
2019-05-10 20:19:04 +1000

    add

---
+++ KnowledgeBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ai_ass2
+{
+    public class KnowledgeBase
+    {
+        private List<Sentence> _sentences;
+        private List<Symbol> _symbols;
+
+        public KnowledgeBase(string[] sentences)
+        {
+            foreach(string s in sentences)
+            {
+                Sentence sentence = new Sentence(s);
+                _sentences.Add(sentence);
+                _symbols.AddRange(sentence.AllSymbols);
+            }
+        }
+    }
+}
---
+++ Symbol.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ai_ass2
+{
+    public class Symbol
+    {
+        string _name;
+        bool _value;
+
+        public Symbol(string name)
+        {
+            _name = name;
+            _value = false;
+        }
+
+        public string Name { get => _name; set => _name = value; }
+        public bool Value { get => _value; set => _value = value; }
+    }
+}
